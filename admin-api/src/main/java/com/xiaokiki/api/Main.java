8e99268a95119002c075dca273231d7f503c5fac
onlylonger <onlylonger@outlook.com>
2024-09-25 22:22:58 +0800

    U

---
+++ admin-api/src/main/java/com/xiaokiki/api/Main.java
@@ -0,0 +1,11 @@
+package com.xiaokiki.api;
+
+
+import com.xiaokiki.json.Json;
+
+public class Main {
+    public static void main(String[] args) {
+        Json.parse();
+        System.out.println("hello");
+    }
+}
---
+++ json/src/main/java/com/xiaokiki/json/Json.java
@@ -0,0 +1,8 @@
+package com.xiaokiki.json;
+
+public final class Json {
+    public static String parse() {
+        System.out.println("nihao");
+        return "123";
+    }
+}
